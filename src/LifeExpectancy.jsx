43c609c1f4975fe0184527533492ae150870298f
ftolentino <filmer.tolentino@gmail.com>
2023-01-12 20:21:31 -0800

    WIP LifeExpectancy component

---
+++ src/LifeExpectancy.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function LifeExpectancy() {
+  const [yearsLeft, setYearsLeft] = useState(0);
+
+  const yearsToLive = (planetAge) => {
+    const averagePlanetLife = 100;
+    
+  } 
+}
+
+export default LifeExpectancy;
